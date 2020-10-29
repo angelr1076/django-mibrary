c04efa502b5b31314892feacf12457a4719491a1
Angel Rodriguez <angelr1076@gmail.com>
2020-10-29 19:19:56 -0400

    Updated static root

--- virtual_library/settings.py
+++ virtual_library/settings.py
@@ -142,6 +142,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
