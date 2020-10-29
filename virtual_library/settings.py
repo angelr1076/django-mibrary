79be3ed3ef7967f86d693f60948d00d8b4f7c38b
Angel Rodriguez <angelr1076@gmail.com>
2020-10-29 19:44:06 -0400

    Updated allowed hosts

--- virtual_library/settings.py
+++ virtual_library/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG_VALUE")
 
-ALLOWED_HOSTS = ['https://mibrary-library.herokuapp.com/']
+ALLOWED_HOSTS = ["mibrary-library.herokuapp.com/"]
 
 # Application definition
 
