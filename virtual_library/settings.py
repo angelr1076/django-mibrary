d744bedba3361fa0c5cf10e1c0105166d14b7ee8
Angel Rodriguez <angelr1076@gmail.com>
2020-11-28 11:34:53 -0500

    added env

--- virtual_library/settings.py
+++ virtual_library/settings.py
@@ -145,7 +145,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
