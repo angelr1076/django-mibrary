f4ae54cb75e704b4ae78031c81ea5ff495eaec97
angelr1076 <angelr1076@gmail.com>
2023-04-11 11:49:54 -0400

    Set the debug value back to the environ settings for production

--- virtual_library/settings.py
+++ virtual_library/settings.py
@@ -42,8 +42,8 @@ if os.environ.get('DJANGO_ENV') == 'local':
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
 
 # DEBUG = 'True'
 
@@ -126,9 +126,6 @@ DATABASES = {
         'PORT': PGPORT,
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
